be82e60c2117854039c31d352ee0a0bfa528f314
Chantel <chantelfrizzell@hotmail.com>
2015-02-08 16:16:51 -0800

    initial commit

--- Is_it_prime/ViewController.swift
+++ Is_it_prime/ViewController.swift
@@ -10,6 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numberBox: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func buttonClicked(sender: AnyObject) {
+        
+        var numberInt = numberBox.text.toInt()
+        
+        if numberInt != nil {
+            
+            var unwrappedNumber = numberInt!
+            var isPrime = true
+            
+        if unwrappedNumber == 1 {
+                
+             var isPrime = false
+        }
+            
+        if unwrappedNumber != 2 && unwrappedNumber != 1 {
+            for var i = 2; i < unwrappedNumber; i++ {
+            
+            if unwrappedNumber % 1 == 0 {
+                isPrime = false
+                }
+            }
+        }
+            
+        if isPrime == true {
+                resultLabel.text = "\(unwrappedNumber) is prime!"
+        }
+            
+         else {
+            
+            resultLabel.text = "\(unwrappedNumber) is not prime"
+        }
+    }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
